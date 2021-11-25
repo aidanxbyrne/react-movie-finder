bee99309cf68d76d738bceeffa1071cae90b9d1d
Aidan Byrne <aidanbyrne142@gmail.com>
2021-11-25 18:50:31 +0000

    Modal CSS Changes
    - Moved a number of Modal CSS stylings from Bootstrap classes to the main stylesheet

--- src/components/MovieModal.js
+++ src/components/MovieModal.js
@@ -5,30 +5,30 @@ const MovieModal = ({selectedMovie, convertedDate}) => {
 
 
     return (
-        <div className="position-fixed w-100 h-100 top-0 start-0 d-flex align-items-center justify-content-center" style={{zIndex: 1, backgroundColor: 'rgba(0, 0, 0, 0.5)'}}>
-            <div className="card d-flex flex-row position-relative rounded-3 p-0 m-3 w-100" style={{ height: '600px', maxWidth: '1024px'}}>
-                <div className="modal-image">
+        <div className="modal-main">
+            <div className="modal-card card ">
+                <div className="modal-card-image">
                     <img src={selectedMovie.poster_path ? `https://image.tmdb.org/t/p/w342/${selectedMovie.poster_path}` : NotFoundImage} className="img h-100 rounded-start" alt="..." />
                 </div>
-                <div className="card-body p-4">
-                    <div className="selected-content selected-content-head">
-                        <div className="selected-content-head-left">
+                <div className="modal-card-body card-body p-4">
+                    <div className="modal-card-content modal-content-head">
+                        <div className="modal-content-head-left">
                             <h2>{selectedMovie.original_title}</h2>
                             <p>{convertedDate}</p>
                         </div>
-                        <div className="selected-content-head-right selected-tags"></div>
+                        <div className="modal-content-head-right modal-tags"></div>
                     </div>
-                    <div className="selected-content">
+                    <div className="smodal-card-content">
                         <h3>Overview</h3>
                         <p>{selectedMovie.overview}</p>
                     </div>
-                    <div className="selected-content">
+                    <div className="modal-card-content">
                         <h4>Director</h4>
-                        <p>selectedMovie.original_title</p>
+                        <p>{selectedMovie.original_title}</p>
                         <h4>Language</h4>
-                        <p>selectedMovie.original_title</p>
+                        <p>{selectedMovie.original_title}</p>
                         <h4>Budget</h4>
-                        <p>selectedMovie.original_title</p>
+                        <p>{selectedMovie.original_title}</p>
                     </div>
                 </div>
             </div>
